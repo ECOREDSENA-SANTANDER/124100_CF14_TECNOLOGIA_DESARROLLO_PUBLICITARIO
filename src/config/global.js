a17be3bba0ff5ffc4c136a473d66fc6bea4d5c15
Andrea Botello <andrea.botellor@misena.edu.co>
2024-09-23 08:44:04 -0500

    CFA y Material

--- src/config/global.js
+++ src/config/global.js
@@ -110,7 +110,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF14_124100_DU.pdf',
       },
       {
         icono: 'fas fa-download',
